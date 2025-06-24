2fe326a43c0f8ca7ad5d45db00720cb8d59689d6
fwc <1643806656@qq.com>
2025-06-24 21:24:21 +0800

    config: update username and comment out unused imports

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -5,16 +5,16 @@
     ./hardware.nix
     ./xserver.nix
     ./network.nix
-    ./nh.nix
+    # ./nh.nix
     ./pipewire.nix
     ./program.nix
     ./security.nix
     ./services.nix
-    ./steam.nix
+    # ./steam.nix
     ./system.nix
     ./flatpak.nix
     ./user.nix
     ./wayland.nix
-    ./virtualization.nix
+    # ./virtualization.nix
   ];
 }
